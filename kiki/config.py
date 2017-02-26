86db4d888e616bb3fa29746f7b9c1604983f6b1d
SeokYoung Kim <crom@zzizily.com>
2017-02-27 05:09:30 +0900

    기존 local_config.ini 파일을 읽어서 configparser 이용하던 방법에서 config.py 만들어서 dict 로 사용하기 mkae_config.py 를 이용해서 암호화 해서 파일 만들기 make_config.py 는 git 에 추가 하지 않는다

---
+++ kiki/config.py
@@ -0,0 +1,27 @@
+config = {
+    "db": {
+        "redis.db.common": 0,
+        "redis.host": "gAAAAABYsy2id7Px67w1V5jaJgZ1LWJUnhK6Hp400YBzZSutABxiCejce2uRZXndXu5hzDhkIZ4gmPu63lPwL8MeS389nTIJ_fYXMfvH4iTlbr1D47AE74Y=",
+        "redis.password": "gAAAAABYsy2iHIwY0bR9wSaT_lrv8FvbuctlgG1tNbEB4LiNgh5Pqn8cFmyaYXjmem73Drp0xY2C4xRQt_og0FOx6tnJPc6BDw==",
+        "redis.port": "gAAAAABYsy2inASmRceV1Hf6hgdVgutPr1MLpD3WnSezeAVak3x5iUK4898OPGKTv19Lvgv25dtcC31jYF9QtFhYeJnxv5f2PA=="
+    },
+    "default": {
+        "mode": "local"
+    },
+    "log": {
+        "file.size": 128,
+        "formatter": "[%%(levelname)s|%%(filename)s:%%(lineno)s] %%(asctime)s > %%(message)s",
+        "path": "D:/DK/Version/Git/ZZiZiLY/Python/kiki/logs",
+        "prefix": "%%Y/%%m/%%d",
+        "suffix": "%%H%%M%%S"
+    },
+    "slack": {
+        "chcnnel": "#carpediem",
+        "token": "gAAAAABYsy2iQ_x2zDkx4-xq3T9HRyHROvJJk4Yqm0S7HqOB2kMokXdGEAZ2V98JDOUtdfVs-y0WxvcAjGLprbGiqcZNybao2qQLBPrnlWNMd9Ed0JinYoV6r3Ib1h5XFnzE9ujYVFUe",
+        "username": "carpediem"
+    },
+    "telegram": {
+        "chat_id": "gAAAAABYsy2i2SfTkMXBlm9FxUCMozdGUFk4cCdbkfNKDj0grsnR-yV1yLDZAb3XLT0whVryGwzFk7hHgasrWMxO839URvtwew==",
+        "token": "gAAAAABYsy2iQ_x2zDkx4-xq3T9HRyHROvJJk4Yqm0S7HqOB2kMokXdGEAZ2V98JDOUtdfVs-y0WxvcAjGLprbGiqcZNybao2qQLBPrnlWNMd9Ed0JinYoV6r3Ib1h5XFnzE9ujYVFUe"
+    }
+}
--- kiki/settings.py
+++ kiki/settings.py
@@ -1,19 +1,14 @@
 import os
-from configparser import ConfigParser
 
 import redis
 from cryptography.fernet import Fernet
 
-__version__ = (0, 0, 1)
+from kiki.config import config
 
 crypto = Fernet(os.getenv('ZZIZILY_KIKI_CRYPTO'))
-
-config = ConfigParser()
-config.read('./resources/{mode}_config.ini'.format(mode=os.getenv('ZZIZILY_KIKI_MODE')))
-
 redis_pool_common = redis.ConnectionPool(
-    host=crypto.decrypt(config.get('db', 'redis.host').encode()).decode(),
-    port=int(crypto.decrypt(config.get('db', 'redis.port').encode()).decode()),
-    password=crypto.decrypt(config.get('db', 'redis.password').encode()).decode(),
-    db=int(config.get('db', 'redis.db.common')),
+    host=crypto.decrypt(config['db']['redis.host'].encode()).decode(),
+    port=int(crypto.decrypt(config['db']['redis.port'].encode()).decode()),
+    password=crypto.decrypt(config['db']['redis.password'].encode()).decode(),
+    db=int(config['db']['redis.db.common']),
 )
