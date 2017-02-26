5f5b499bab048d4786f9731a8a934071052e0508
SeokYoung Kim <crom@zzizily.com>
2017-02-27 07:34:00 +0900

    간단한 수정

--- kiki/settings.py
+++ kiki/settings.py
@@ -4,8 +4,8 @@ from kiki import crypto
 from kiki.config import config
 
 redis_pool_common = redis.ConnectionPool(
-    host=crypto.decrypt(config['db']['redis.host'].encode()).decode(),
-    port=int(crypto.decrypt(config['db']['redis.port'].encode()).decode()),
-    password=crypto.decrypt(config['db']['redis.password'].encode()).decode(),
-    db=int(config['db']['redis.db.common']),
+    host=crypto.decrypt(config['db']['redis_host'].encode()).decode(),
+    port=int(crypto.decrypt(config['db']['redis_port'].encode()).decode()),
+    password=crypto.decrypt(config['db']['redis_password'].encode()).decode(),
+    db=int(config['db']['redis_db_common']),
 )
