2ee98f1f1895e62a86ae7bacd177e9a01d9a364a
SeokYoung Kim <crom@zzizily.com>
2017-02-27 07:32:45 +0900

    구분자값 . 에서 _ 변경에 따른 소스 변경

--- kiki/commons/log/__init__.py
+++ kiki/commons/log/__init__.py
@@ -8,19 +8,19 @@ def get_logger(logger, level=logging.DEBUG, path='../log', prefix='%Y/%m/%d', su
                encoding='utf-8', config=None):
     now = time.localtime()
 
-    folder = '{}/{}'.format(config and config.get('log', 'path') or path,
-                            time.strftime(config and config.get('log', 'prefix') or prefix, now))
-    filename = '{}.{}.log'.format(logger.name, time.strftime(config and config.get('log', 'suffix') or suffix, now))
-    logger_formatter = logging.Formatter(config and config.get('log', 'formatter') or formatter)
+    folder = '{}/{}'.format(config and config['log']['path'] or path,
+                            time.strftime(config and config['log']['prefix'] or prefix, now))
+    filename = '{}.{}.log'.format(logger.name, time.strftime(config and config['log']['suffix'] or suffix, now))
+    logger_formatter = logging.Formatter(config and config['log']['formatter'] or formatter)
 
     if not os.path.exists(folder):
         os.makedirs(folder)
 
     # MB 기준
-    fileMaxByte = 1024 * 1024 * (config and int(config.get('log', 'file.size')) or file_size)
+    fileMaxByte = 1024 * 1024 * (config and int(config['log']['file_size']) or file_size)
     fileHandler = logging.handlers.RotatingFileHandler(filename='{}/{}'.format(folder, filename), maxBytes=fileMaxByte,
                                                        backupCount=10,
-                                                       encoding=config and config.get('log', 'encoding') or encoding)
+                                                       encoding=config and config['log']['encoding'] or encoding)
     streamHandler = logging.StreamHandler()
 
     fileHandler.setFormatter(logger_formatter)
