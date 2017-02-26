86362e1fa9efbd03f6dd001136638033ead6bc54
SeokYoung Kim <crom@zzizily.com>
2017-02-27 08:17:43 +0900

    config log 값 %% 중복으로된것 % 로 수정

--- kiki/config.py
+++ kiki/config.py
@@ -1,27 +1,28 @@
 config = {
     "db": {
         "redis_db_common": 0,
-        "redis_host": "gAAAAABYs1HkubtByYkG8k3L3_tb8OWSbmAdEa-xe2dveLDYBGRppiT6SOyavkdv2Qx_J5wQZhh8ox3t0hkBT5gkN26j_BUAkDE5-xGtJSmxJ8O69arnrTk=",
-        "redis_password": "gAAAAABYs1Hkoao3RqDiNludl5eq0XJ_0OGPK6Tj_GnYMSx0Zrcm9vJ_PFZbUoSxwuZfZWDd9L2IcxvSW0jKYRRDQV3te2chlQ==",
-        "redis_port": "gAAAAABYs1HkNDBWA7OoOXzHo43DzfKwP10FWamsDCnKTZIz7ShdFNhCnUUJnQril6y3nitFqtPJMjYCX1CeO0Em8u3Okp5EaQ=="
+        "redis_host": "gAAAAABYs2DAXC2AMKIe38359Oq2aQs6lasZzSWieP63zaT5RmHARgeYyc6rQ_PARs0kaKPCPkRjZN-ZOkgzxhCuWp3sh01RCESCqe9RPWZSB-1d6BUeHNM=",
+        "redis_password": "gAAAAABYs2DAREKNfZAKUA2SyuF82SV7E4_Jh8NACBIKQ1duoI2ceGhVrBczDlEM0oM6yjKgablgsGk6jttGpDi5Fm9Mu1QXjQ==",
+        "redis_port": "gAAAAABYs2DAtYPVa65DHH-L1FAJlvoc95PNBKYwGdk6F0LANuX74Y_RrglvARB7tV_rUPl_ReeEdMgIP7lpCWnkWiNf54vuqg=="
     },
     "default": {
         "mode": "local"
     },
     "log": {
-        "file.size": 128,
-        "formatter": "[%%(levelname)s|%%(filename)s:%%(lineno)s] %%(asctime)s > %%(message)s",
+        "encoding": "utf-8",
+        "file_size": 128,
+        "formatter": "[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s",
         "path": "D:/DK/Version/Git/ZZiZiLY/Python/kiki/logs",
-        "prefix": "%%Y/%%m/%%d",
-        "suffix": "%%H%%M%%S"
+        "prefix": "%Y/%m/%d",
+        "suffix": "%H%M%S"
     },
     "slack": {
         "chcnnel": "#carpediem",
-        "token": "gAAAAABYs1HknuZcMdv9SyxAZWTKSAlpUUfpIkUgwLIfWpDD18J41ARvAL5tK0zxINfnC_FWgFizpqVJ4peJIElc2ynijqJJ3OeG1FozQgKEe7s13aoxvPF8jxMZlJmYrWVYHc-VB__O",
+        "token": "gAAAAABYs2DAkTOd42_eKxlO1inm3ZB8PrBkgianAglC-GyBZb2xTxdqVmJQzTd3drUfbR0qEi1jXOv-h2h6vL_l6Z02927QLtPuPORMxZtuGw2JMAL6GL9DUiUGHuRjrmzZiQ38Sp37",
         "username": "carpediem"
     },
     "telegram": {
-        "chat_id": "gAAAAABYs1HkcLn3inowHJhiQDQoFxJFheADrARvEJiNIpax3Fq8sszSgcUM8dOf4EvY45Pm_Y02VCD4kM96WGfZdcFaGwEM8g==",
-        "token": "gAAAAABYs1HknuZcMdv9SyxAZWTKSAlpUUfpIkUgwLIfWpDD18J41ARvAL5tK0zxINfnC_FWgFizpqVJ4peJIElc2ynijqJJ3OeG1FozQgKEe7s13aoxvPF8jxMZlJmYrWVYHc-VB__O"
+        "chat_id": "gAAAAABYs2DA8wTW7ob9Y_qzJPRkpfK5zslzx8W6Q5Ar-DQOfXb7n_oeyO4XMfbDlSV2Cu8pWPrE1iglKaPMI6OATpuhXgwwJw==",
+        "token": "gAAAAABYs2DAkTOd42_eKxlO1inm3ZB8PrBkgianAglC-GyBZb2xTxdqVmJQzTd3drUfbR0qEi1jXOv-h2h6vL_l6Z02927QLtPuPORMxZtuGw2JMAL6GL9DUiUGHuRjrmzZiQ38Sp37"
     }
 }
