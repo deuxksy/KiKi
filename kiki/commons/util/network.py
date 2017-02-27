a5835142608f3b56c62845a22511917c2cd75cb7
SeokYoung Kim <crom@zzizily.com>
2017-02-28 07:45:41 +0900

    ip 가지고 오기 proxies 이용도 가능

--- kiki/commons/util/network.py
+++ kiki/commons/util/network.py
@@ -1,5 +1,12 @@
 import requests
+from requests.exceptions import ProxyError
 
 
-def get_ip():
-    return requests.get('http://ifconfig.co/json').json().get('ip')
+def get_ip(proxies=None):
+    ip = None
+    try:
+        ip = requests.get('https://ifconfig.co/json', proxies=proxies).json().get('ip')
+    except ProxyError:
+        ip = requests.get('https://ifconfig.co/json', proxies=proxies).json().get('ip')
+    finally:
+        return ip
