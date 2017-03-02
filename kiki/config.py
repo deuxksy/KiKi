8018ac92621c958b17710596e083386c0f3b4fc8
SeokYoung Kim <crom@zzizily.com>
2017-03-02 21:45:29 +0900

    confog.py 를 만드는 make_config.py project_home 인자가 추가

--- kiki/config.py
+++ kiki/config.py
@@ -2,9 +2,9 @@
 config = {
     "db": {
         "redis_db_common": 0,
-        "redis_host": "gAAAAABYuAbL3ggpn2RMNTUt0Wcx7XS6bN80J28FxScZRYp1NMzTD5lCEHCT4dlNLjVvG-_LftRNzl3kb1_my1RyTlGEBd4ekyo1zyoKHhRhCBcU22Ex5jI=",
-        "redis_password": "gAAAAABYuAbLQEFOHf7zJrMP4OuaSJyGrmnHHSwzzv2S98AajJs46bmS0CFuqYH-nRobMO1rrgVl2VB8ZeD4OQIyEsrAYkcwmQ==",
-        "redis_port": "gAAAAABYuAbL4i1syTlAv2gVNvRNo2p6xeYbugyfTm6T1ygXxGCzIXLhai2AIsKT6vgRooBMBJa95hXQRdGtEIZ1CEAYAUA0kg=="
+        "redis_host": "gAAAAABYuBGisysMwaP6LLX3ha8NEpPH1JawVRlh9o_erGPah5ElNuHzYmbq2DAw8z9tNviPIpOIPmCk9RINSn0NxowDKfZXoIYL5zQNaBS862tAit4KnJE=",
+        "redis_password": "gAAAAABYuBGiB8eQimzbpgW_kcHdcDDiAM_UnlW3qvZZ8KmMMbAnoORW7e2DSGSU8EwreWj0Rulv3PXIAb6LpXeWGmDGf8840A==",
+        "redis_port": "gAAAAABYuBGi0BMOxCyS3XuOxUtZC93cTQbfZLcztlo-QsO09cgDDRRK8rqO8oWRsPvmU22WuaJ_0YMZL31o-MM5V0WVk13zWA=="
     },
     "default": {
         "mode": "local"
@@ -19,11 +19,11 @@ config = {
     },
     "slack": {
         "chcnnel": "#carpediem",
-        "token": "gAAAAABYuAbLcFsdDvHqs6AVVRM8eGF79ISMT3CwyExs4MhzNzhtBb9EsTZOXBMOosLC3GUCMTlKtYzJx_JI_0x4eDxRfgLii5-G9nVHzO_SLk1byr2VZpzQfR2IjEwxeI3hPc7o1dyg",
+        "token": "gAAAAABYuBGirpEVk9Ldq40VD8rvkbNTO1lH25-hrpgoHZqCLRubA_hpHiTxzjl8TYdZ9vTtLrmpoQztREQnCf2-1dmrxnWv2oOOKTW43eSodeL0AwYVoEsp_PEgkDY199jKWZW_1sJ5",
         "username": "carpediem"
     },
     "telegram": {
-        "chat_id": "gAAAAABYuAbL_pF8IRSMJqP9oCVQ1_QkiELKgdjCBRSxz8MdNUogaZCXHxlP2dlUm2fbyHWvClV0LhwVbyiksokgrCtb8zD2kg==",
-        "token": "gAAAAABYuAbLcFsdDvHqs6AVVRM8eGF79ISMT3CwyExs4MhzNzhtBb9EsTZOXBMOosLC3GUCMTlKtYzJx_JI_0x4eDxRfgLii5-G9nVHzO_SLk1byr2VZpzQfR2IjEwxeI3hPc7o1dyg"
+        "chat_id": "gAAAAABYuBGi2mb135f0xdtwBIA6zhxwC6aW8fy93v09-mGSEcrf75l2FygNJVQOTO5vugPp62YHRqcxUK5xNgkYw-s62f-D9Q==",
+        "token": "gAAAAABYuBGirpEVk9Ldq40VD8rvkbNTO1lH25-hrpgoHZqCLRubA_hpHiTxzjl8TYdZ9vTtLrmpoQztREQnCf2-1dmrxnWv2oOOKTW43eSodeL0AwYVoEsp_PEgkDY199jKWZW_1sJ5"
     }
 }
